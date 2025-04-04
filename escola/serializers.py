109b462d1d5cfd31c577b82d719722ef1b8cc485
berllocK <brenosouzaemp@gmail.com>
2025-04-04 00:47:37 -0300

    Create second version API

--- escola/serializers.py
+++ escola/serializers.py
@@ -7,13 +7,13 @@ class EstudanteSerializer(serializers.ModelSerializer):
         model = Estudante
         fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
 
-    def validate(self, dados):
+    def validate(self,dados):
         if cpf_invalido(dados['cpf']):
-            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 digitos!'})   
+            raise serializers.ValidationError({'cpf':'O CPF deve ter um valor válido'})
         if nome_invalido(dados['nome']):
-            raise serializers.ValidationError({'nome': 'O nome so pode ter letras'})
+            raise serializers.ValidationError({'nome':'O nome só pode ter letras'})
         if celular_invalido(dados['celular']):
-               raise serializers.ValidationError({'celular': 'O celar precisa ter 13 digitos'})
+            raise serializers.ValidationError({'celular':'O celular precisa seguir o modelo: 86 99999-9999 (respeitando traços e espaços)'})
         return dados
 
 
@@ -41,3 +41,8 @@ class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         fields = ['estudante_nome']
+
+class EstudanteSerializerV2(serializers.ModelSerializer):
+    class Meta:
+        model = Estudante
+        fields = ['id', 'nome', 'email', 'celular']
